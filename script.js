a9935e96458c8ca31abcf4037476315c13772f76
yayoi2026-menu <koyuki256@gmail.com>
2026-08-06 18:17:04 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -20,10 +20,25 @@ const breakfastMenuTue = document.getElementById("breakfast-menu-tue");
 const breakfastWed = document.getElementById("breakfast-wed");
 const breakfastMenuWed = document.getElementById("breakfast-menu-wed");
 
+const breakfastThu = document.getElementById("breakfast-thu");
+const breakfastMenuThu = document.getElementById("breakfast-menu-thu");
+
+const breakfastFri = document.getElementById("breakfast-fri");
+const breakfastMenuFri = document.getElementById("breakfast-menu-fri");
+
+const breakfastSat = document.getElementById("breakfast-sat");
+const breakfastMenuSat = document.getElementById("breakfast-menu-sat");
+
+const breakfastSun = document.getElementById("breakfast-sun");
+const breakfastMenuSun = document.getElementById("breakfast-menu-sun");
+
 let breakfastItems = [];
 let breakfastItemsTue = [];
 let breakfastItemsWed = [];
-
+let breakfastItemsThu = [];
+let breakfastItemsFri = [];
+let breakfastItemsSat = [];
+let breakfastItemsSun = [];
 
 breakfastMon.addEventListener("click", function () {
 
@@ -40,6 +55,26 @@ breakfastWed.addEventListener("click", function () {
     breakfastMenuWed.style.display = "block";
 });
 
+breakfastThu.addEventListener("click", function () {
+
+    breakfastMenuThu.style.display = "block";
+});
+
+breakfastFri.addEventListener("click", function () {
+
+    breakfastMenuFri.style.display = "block";
+});
+
+breakfastSat.addEventListener("click", function () {
+
+    breakfastMenuSat.style.display = "block";
+});
+
+breakfastSun.addEventListener("click", function () {
+
+    breakfastMenuSun.style.display = "block";
+});
+
 
 const breadOption = document.getElementById("bread-option");
 
@@ -51,6 +86,18 @@ const breakfastOptionsTue =
 
 const breakfastOptionsWed =
     document.querySelectorAll("#breakfast-menu-wed div:not(#back-option-wed):not(#done-option-wed)");  
+
+const breakfastOptionsThu =
+    document.querySelectorAll("#breakfast-menu-thu div:not(#back-option-thu):not(#done-option-thu)");
+
+const breakfastOptionsFri =
+    document.querySelectorAll("#breakfast-menu-fri div:not(#back-option-fri):not(#done-option-fri)");  
+
+const breakfastOptionsSat =
+    document.querySelectorAll("#breakfast-menu-sat div:not(#back-option-sat):not(#done-option-sat)");
+
+const breakfastOptionsSun =
+    document.querySelectorAll("#breakfast-menu-sun div:not(#back-option-sun):not(#done-option-sun)");  
 
 
 breakfastOptions.forEach(function(option) {
@@ -76,6 +123,7 @@ breakfastOptions.forEach(function(option) {
 
 });
 
+
 breakfastOptionsTue.forEach(function(option) {
 
     option.addEventListener("click", function() {
@@ -98,6 +146,7 @@ breakfastOptionsTue.forEach(function(option) {
     });
 
 });
+
 
 breakfastOptionsWed.forEach(function(option) {
 
@@ -122,6 +171,103 @@ breakfastOptionsWed.forEach(function(option) {
 
 });
 
+
+breakfastOptionsThu.forEach(function(option) {
+
+    option.addEventListener("click", function() {
+
+        const emoji = option.textContent.split(" ")[0];
+
+        if (emoji === "✔") {
+            breakfastMenuThu.style.display = "none";
+            return;
+        }
+
+        if (breakfastItemsThu.length < 3) {
+
+            breakfastItemsThu.push(emoji);
+
+            breakfastThu.textContent = breakfastItemsThu.join("");
+
+        }
+
+    });
+
+});
+
+
+breakfastOptionsFri.forEach(function(option) {
+
+    option.addEventListener("click", function() {
+
+        const emoji = option.textContent.split(" ")[0];
+
+        if (emoji === "✔") {
+            breakfastMenuFri.style.display = "none";
+            return;
+        }
+
+        if (breakfastItemsFri.length < 3) {
+
+            breakfastItemsFri.push(emoji);
+
+            breakfastFri.textContent = breakfastItemsFri.join("");
+
+        }
+
+    });
+
+});
+
+
+breakfastOptionsSat.forEach(function(option) {
+
+    option.addEventListener("click", function() {
+
+        const emoji = option.textContent.split(" ")[0];
+
+        if (emoji === "✔") {
+            breakfastMenuSat.style.display = "none";
+            return;
+        }
+
+        if (breakfastItemsSat.length < 3) {
+
+            breakfastItemsSat.push(emoji);
+
+            breakfastSat.textContent = breakfastItemsSat.join("");
+
+        }
+
+    });
+
+});
+
+
+breakfastOptionsSun.forEach(function(option) {
+
+    option.addEventListener("click", function() {
+
+        const emoji = option.textContent.split(" ")[0];
+
+        if (emoji === "✔") {
+            breakfastMenuSun.style.display = "none";
+            return;
+        }
+
+        if (breakfastItemsSun.length < 3) {
+
+            breakfastItemsSun.push(emoji);
+
+            breakfastSun.textContent = breakfastItemsSun.join("");
+
+        }
+
+    });
+
+});
+
+
 const doneOption = document.getElementById("done-option");
 const backOption = document.getElementById("back-option");
 
@@ -130,6 +276,18 @@ const backOptionTue = document.getElementById("back-option-tue");
 
 const doneOptionWed = document.getElementById("done-option-wed");
 const backOptionWed = document.getElementById("back-option-wed");
+
+const doneOptionThu = document.getElementById("done-option-thu");
+const backOptionThu = document.getElementById("back-option-thu");
+
+const doneOptionFri = document.getElementById("done-option-fri");
+const backOptionFri = document.getElementById("back-option-fri");
+
+const doneOptionSat = document.getElementById("done-option-sat");
+const backOptionSat = document.getElementById("back-option-sat");
+
+const doneOptionSun = document.getElementById("done-option-sun");
+const backOptionSun = document.getElementById("back-option-sun");
 
 
 doneOption.addEventListener("click", function () {
@@ -189,6 +347,90 @@ backOptionWed.addEventListener("click", function () {
         breakfastItemsWed.pop();
 
         breakfastWed.textContent = breakfastItemsWed.join("");
+
+    }
+
+});
+
+
+doneOptionThu.addEventListener("click", function () {
+
+    breakfastItemsThu = [];
+
+    breakfastMenuThu.style.display = "none";
+
+});
+
+backOptionThu.addEventListener("click", function () {
+
+    if (breakfastItemsThu.length > 0) {
+
+        breakfastItemsThu.pop();
+
+        breakfastThu.textContent = breakfastItemsThu.join("");
+
+    }
+
+});
+
+
+doneOptionFri.addEventListener("click", function () {
+
+    breakfastItemsFri = [];
+
+    breakfastMenuFri.style.display = "none";
+
+});
+
+backOptionFri.addEventListener("click", function () {
+
+    if (breakfastItemsFri.length > 0) {
+
+        breakfastItemsFri.pop();
+
+        breakfastFri.textContent = breakfastItemsFri.join("");
+
+    }
+
+});
+
+
+doneOptionSat.addEventListener("click", function () {
+
+    breakfastItemsSat = [];
+
+    breakfastMenuSat.style.display = "none";
+
+});
+
+backOptionSat.addEventListener("click", function () {
+
+    if (breakfastItemsSat.length > 0) {
+
+        breakfastItemsSat.pop();
+
+        breakfastSat.textContent = breakfastItemsSat.join("");
+
+    }
+
+});
+
+
+doneOptionSun.addEventListener("click", function () {
+
+    breakfastItemsSun = [];
+
+    breakfastMenuSun.style.display = "none";
+
+});
+
+backOptionSun.addEventListener("click", function () {
+
+    if (breakfastItemsSun.length > 0) {
+
+        breakfastItemsSun.pop();
+
+        breakfastSun.textContent = breakfastItemsSun.join("");
 
     }
 
